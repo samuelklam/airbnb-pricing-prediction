6884715930b7681c521a505eee68026eb8a4b2aa
Samuel Lam <samuelkinlam@gmail.com>
2016-12-26 12:21:34 -0500

    add in scoped for rest of jupyter pages

--- public/js/page-jupyter-control.js
+++ public/js/page-jupyter-control.js
@@ -2,13 +2,12 @@ function includeContent(file) {
   var URL = window.location.href;
   URL = URL.substring(0, URL.lastIndexOf("/") + 1) + file;
   $(function(){
-    $("#includedContent").load(URL,function(){
-    	$.scoped();
+    $("#includedContent").load(URL, function() {
+        $.scoped();
     });
   });
 }
 
 $(function() {
     includeContent("jupyter-notebooks/introduction.html");
-    
 });
