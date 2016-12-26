aff10599b5ac2308857d61684162d57340a63b53
Samuel Lam <samuelkinlam@gmail.com>
2016-12-26 13:34:33 -0500

    refactor jupyter control

--- public/js/page-jupyter-control.js
+++ public/js/page-jupyter-control.js
@@ -1,11 +1,9 @@
 function includeContent(file) {
-  var URL = window.location.href;
-  URL = URL.substring(0, URL.lastIndexOf("/") + 1) + file;
-  $(function(){
+    var URL = window.location.href;
+    URL = URL.substring(0, URL.lastIndexOf("/") + 1) + file;
     $("#includedContent").load(URL, function() {
         $.scoped();
     });
-  });
 }
 
 $(function() {
